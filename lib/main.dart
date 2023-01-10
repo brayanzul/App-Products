b9904475f74eed384042f575e4c4bbe4f1b98f3f
Brayan Zuluaga <brayanzuluaga93@hotmail.com>
2023-01-10 13:00:04 -0500

    Add the Producros App project and work today: Home Screen and Login Screen

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:productos_app/screens/screens.dart';
+
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Productos App',
+      initialRoute: 'login',
+      routes: {
+        'login': ( _ ) => LoginScreen(),
+        'home' : ( _ ) => HomeScreen(),
+      },
+    );
+  }
+}
