d41db31890cbacb6cfc3d770d1fdbdd349ff2be5
Brayan Zuluaga <brayanzuluaga93@hotmail.com>
2023-01-17 21:55:55 -0500

    Add the Products App project and today work on: Product, Home Screen, Products Service

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:productos_app/services/services.dart';
 import 'package:productos_app/widgets/widgets.dart';
+import 'package:provider/provider.dart';
 
 
 class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
+    final productsService = Provider.of<ProductsService>(context);
+
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
@@ -15,7 +20,7 @@ class HomeScreen extends StatelessWidget {
         itemCount: 10,
         itemBuilder: ( BuildContext context, int index ) => GestureDetector(
           onTap: () => Navigator.pushNamed(context, 'product'),
-          child: ProductCard()
+          child: const ProductCard()
         ),
       ),
       floatingActionButton: FloatingActionButton(
--- lib/services/products_service.dart
+++ lib/services/products_service.dart
@@ -1,12 +1,36 @@
+import 'dart:convert';
+
 import 'package:flutter/cupertino.dart';
 import 'package:productos_app/models/models.dart';
+import 'package:http/http.dart' as http;
 
 class ProductsService extends ChangeNotifier {
 
-  final String _baseUrl = 'flutter-varios-b7784-default-rtdb.firebaseio.com';
+  final String _baseUrl = 'https://flutter-varios-b7784-default-rtdb.firebaseio.com';
   final List<Product> products = [];
+  bool isLoading = true;
 
-  //TODO: hacer fetch de productos
+  ProductsService() {
+    this.loadProducts();
+  }
+
+  // TODO: <List<Product>>
+  Future loadProducts() async {
+
+    final url = Uri.https( _baseUrl, 'products.json' );
+    final resp = await http.get(url);
+
+    final Map<String, dynamic> productsMap = json.decode( resp.body );
+
+    productsMap.forEach((key, value) {
+      final tempProduct = Product.fromMap( value );
+      tempProduct.id = key;
+      this.products.add( tempProduct );
+    });
+
+    print( this.products[0].name );
+
+  }
 
 
 }
