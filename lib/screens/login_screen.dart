7dcbd4c8f73ee200ec9cae755dbe9644c1859649
Brayan Zuluaga <brayanzuluaga93@hotmail.com>
2023-02-04 10:08:54 -0500

    Add App Products project and work today: Auth Service, Register Screen, Login Screen

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:productos_app/providers/login_form_provider.dart';
+import 'package:productos_app/services/services.dart';
 import 'package:productos_app/ui/input_decorations.dart';
 import 'package:productos_app/widgets/widgets.dart';
 import 'package:provider/provider.dart';
@@ -122,17 +123,23 @@ class _LoginForm extends StatelessWidget {
                 //TODO: Login form
 
                 FocusScope.of(context).unfocus();
+                final authService = Provider.of<AuthService>( context, listen: false);
 
                 if( !loginForm.isvalidForm() ) return;
 
                 loginForm.isLoading = true;
 
-                await Future.delayed(const Duration(seconds: 2));
 
                 //TODO: validar si el login es correcto
-                loginForm.isLoading = false;
+                final String? errorMesage = await authService.login(loginForm.email, loginForm.password);
 
-                Navigator.pushReplacementNamed(context, 'home');
+                if( errorMesage == null ) {
+                  Navigator.pushReplacementNamed(context, 'home');
+                } else {
+                  // TODO: mostrar error en pantalla
+                  print( errorMesage );
+                  loginForm.isLoading = false;
+                }
               },
               child: Container(
                 padding: const EdgeInsets.symmetric( horizontal: 80, vertical: 15 ),
--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -1,12 +1,16 @@
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+
 import 'package:http/http.dart' as http;
 
 class AuthService extends ChangeNotifier {
 
   final String _baseUrl = 'identitytoolkit.googleapis.com';
   final String _firebaseToken = 'AIzaSyCpqEnDLi7WEJw9xYaTzxNMyh_06yX-sO4';
+
+  final storage = new FlutterSecureStorage();
 
   // Si retornamos algo, es un error, si no, todo bien
   Future<String?> createUser( String email, String password ) async {
@@ -25,11 +29,37 @@ class AuthService extends ChangeNotifier {
 
     if( decodedResp.containsKey('idToken') ){
       // Token hay que guardarlo en un lugar seguro
+      await storage.write(key: 'token', value: decodedResp['idToken']);
       //decodedResp['idToken'];
       return null;
     } else {
       return decodedResp['error']['message'];
     }
+
+  }
+
+  Future<String?> login( String email, String password ) async {
+
+    final Map<String, dynamic> authData = {
+      'email': email,
+      'password': password
+    };
+
+    final url = Uri.https(_baseUrl, '/v1/accounts:signInWithPassword', {
+      'key': _firebaseToken
+    });
+
+    final resp = await http.post(url, body: json.encode(authData));
+    final Map<String, dynamic> decodedResp = json.decode( resp.body );
+
+    if( decodedResp.containsKey('idToken') ){
+      // Token hay que guardarlo en un lugar seguro
+      //decodedResp['idToken'];
+      await storage.write(key: 'token', value: decodedResp['idToken']);
+      return null;
+    } else {
+      return decodedResp['error']['message'];
+    } 
 
   }
 
